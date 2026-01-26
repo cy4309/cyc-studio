9e5de0df8f690662e4cd9d2c3c8db3af419b078d
Chester Chu <chester@spe3d.co>
2026-01-26 18:38:54 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="fixed inset-0 flex justify-center items-center">
+      <h1 className="text-2xl font-bold text-primary">CYC Studio</h1>
+    </div>
+  );
+}
